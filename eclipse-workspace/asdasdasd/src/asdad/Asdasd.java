bad396c2cad3706f1e128af68e01bbc2bca66c90
Kyle <xkylemcx13@gmail.com>
2019-11-04 09:50:54 +0000

    msg

---
+++ eclipse-workspace/asdasdasd/src/asdad/Asdasd.java
@@ -0,0 +1,14 @@
+package asdad;
+
+public class Asdasd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello World";
+		
+		for(char s : str.toCharArray()) {
+			System.out.println(s);
+		}
+	}
+
+}
